93873f3254e9c3b0724a3c018d1e1443e50a755e
Ikaruga666 <113863223+ArbalestARX7@users.noreply.github.com>
2023-03-05 20:45:50 +0200

    ДЗ 0.01

---
+++ js/01-gallery.js
@@ -0,0 +1,26 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+function makeGallery(arr) {
+  return arr
+    .map(
+      (item) =>
+        `<div class="gallery__item">
+           <a class="gallery__link" href="${item.original}">
+               <img
+                     class="gallery__image"
+                     src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+             </a>
+        </div>`
+    )
+    .join("");
+}
+
+galleryRef.insertAdjacentHTML("beforeend", makeGallery(galleryItems));
